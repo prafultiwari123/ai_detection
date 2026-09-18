84f790ddd17a5f94b7ace7c8be2570a13d32bd4b
praful <Prafultiwari294@gmail.com>
2026-09-18 09:29:08 +0545

    made config

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['@huggingface/transformers', 'onnxruntime-node'],
-    outputFileTracingIncludes: {
-      '/api/detect': ['./node_modules/onnxruntime-node/**/*'],
-    },
+  serverExternalPackages: ['@huggingface/transformers', 'onnxruntime-node', 'onnxruntime-common'],
+  outputFileTracingIncludes: {
+    '/api/detect': [
+      './node_modules/onnxruntime-node/**/*',
+      './node_modules/onnxruntime-common/**/*',
+      './node_modules/@huggingface/transformers/**/*',
+    ],
   },
 };
 
